06cf92f86605e28b6aacd6d007665996aa6ed0a4
Fratu223 <mihaifratu78@gmail.com>
2024-08-15 19:12:48 +0200

    Create dart_tutorial.dart

---
+++ dart_tutorial.dart
@@ -0,0 +1,32 @@
+// for reference: https://www.geeksforgeeks.org/introduction-to-dart-programming-language/
+
+/*
+In dart main() function is a predefined method and acts as the entry point to the application. A dart script 
+needs the main() method for execution of the code. The program code goes like this:
+*/
+
+main() {
+    print("Hello World! Hello Dart");
+}
+
+// for reference: https://www.geeksforgeeks.org/dart-comments/
+
+// This is a single line comment
+
+/* 
+    
+    These are
+
+    Multiple line
+
+    Comments
+
+*/
+
+/// This is
+
+/// A documentation
+
+/// Comment
+
+// for reference: https://dart.dev/language/variables
