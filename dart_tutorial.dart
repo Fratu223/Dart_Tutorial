3ee7c613ba759442315a394ba4c7e5b8a13280bd
Fratu223 <mihaifratu78@gmail.com>
2024-08-17 18:25:07 +0200

    Update dart_tutorial.dart

--- dart_tutorial.dart
+++ dart_tutorial.dart
@@ -30,3 +30,24 @@ main() {
 /// Comment
 
 // for reference: https://dart.dev/language/variables
+
+// Here's an example of creating a variable and initializing it:
+
+var name = 'Bob';
+
+// Variables store references. The variable called name contains a reference to a String object with a value of "Bob".
+
+// The type of the name variable is inferred to be String, but you can change that type by specifying it. If an object isn't
+// restricted to a single type, specify the Object type (or dynamic if necessary).
+
+Object name = 'Bob';
+
+// Another option is to explicitly declare the type that would be inferred:
+
+String name = 'Bob';
+
+// The Dart language enforces sound null safety.
+
+String? name  // Nullable type. Can be `null` or string.
+
+String name   // Non-nullable type. Cannot be `null` but can be string.
