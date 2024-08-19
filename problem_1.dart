303942f5f0c8456ff73258df390d53bd0557ecec
Fratu223 <mihaifratu78@gmail.com>
2024-08-19 17:34:35 +0200

    Update problem_1.dart

---
+++ problem_1.dart
@@ -0,0 +1,16 @@
+void higher(int n, int m) {
+    if (n > m)
+    {
+        print(n);
+    }
+    else
+    {
+        print(m);
+    }
+}
+
+main() {
+    var x = 10;
+    var y = 14;
+    higher(y, x);
+}
