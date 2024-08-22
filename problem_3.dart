e0618e576b9854779b1a04bc5d49a192fea1a8e0
Fratu223 <mihaifratu78@gmail.com>
2024-08-22 17:01:13 +0200

    Update problem_3.dart

--- problem_3.dart
+++ problem_3.dart
@@ -1,9 +1,21 @@
-void higher(String ) {
-    print(n + m);
+// Find the maximum value from a list.
+
+void maxList(List func_list) {
+    int max = 0;
+    int list_length = func_list.length;
+
+    for(int i = 0; i < list_length; i++)
+    {
+        if(func_list[i] > max)
+        {
+            max = func_list[i];
+        }
+    }
+
+    print(max);
 }
 
 main() {
-    var x = 10;
-    var y = 14;
-    higher(y, x);
+    List list = [2, 3, 6, 8, 1];
+    maxList(list);
 }
